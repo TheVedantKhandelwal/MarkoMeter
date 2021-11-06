aa3158521a9477127df9f15f3f8bafb8a42a3b0e
TheVedantKhandelwal <57403056+TheVedantKhandelwal@users.noreply.github.com>
2021-11-06 18:04:09 +0530

    Update show.py

--- show.py
+++ show.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 def show():#To display CSV as a dataframe
-    data = pd.read_csv('C:/Users/khand/Documents/1234.csv')
+    data = pd.read_csv('YOUR PATH HERE')
     data.drop(data.filter(regex="Unnamed:"),axis=1, inplace=True)
     print(data)
 
