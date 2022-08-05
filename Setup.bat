bed8960be9954a83de0bcb8a18b22469d6f256e9
TheVedantKhandelwal <57403056+TheVedantKhandelwal@users.noreply.github.com>
2022-08-05 12:01:02 +0530

    Add files via upload

---
+++ Setup.bat
@@ -0,0 +1,5 @@
+::This script will help install required dependencies and setup Markometer
+
+echo "Physics,Chemistry,Maths" > MARKOMETER.csv
+pip install matplotlib pandas numpy
+python Markometer.py
